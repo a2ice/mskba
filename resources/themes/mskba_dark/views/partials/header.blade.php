c522ef3456d66a181d15f6fb603cf4b5424b017a
Dmitry Losev <no-reply@mskba.ru>
2026-05-14 19:56:23 +0300

    check

--- resources/themes/mskba_dark/views/partials/header.blade.php
+++ resources/themes/mskba_dark/views/partials/header.blade.php
@@ -19,7 +19,7 @@
     <div class="site-header-left header-cell">
         <a class="brand" href="{{ route('home') }}" aria-label="MSKBA">
             <span class="brand__text">
-                <span class="brand__name">[MSK<span>BA</span>]</span>
+                <span class="brand__name">-[MSK<span>BA</span>]-</span>
                 <span class="brand__descriptor"><span class="brand__descriptor-row">баскетбольный портал</span><br> <span class="brand__descriptor-row">Москвы и области</span></span>
             </span>
         </a>
