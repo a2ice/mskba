695e8586edeed0e58a3c110bbc9c3186276d63ad
Dmitry Losev <no-reply@mskba.ru>
2026-05-14 20:04:22 +0300

    check after deploy.yml fix

--- resources/themes/mskba_dark/views/partials/header.blade.php
+++ resources/themes/mskba_dark/views/partials/header.blade.php
@@ -20,7 +20,7 @@
         <a class="brand" href="{{ route('home') }}" aria-label="MSKBA">
             <span class="brand__text">
                 <span class="brand__name">-[MSK<span>BA</span>]-</span>
-                <span class="brand__descriptor"><span class="brand__descriptor-row">баскетбольный портал</span><br> <span class="brand__descriptor-row">Москвы и области</span></span>
+                <span class="brand__descriptor"><span class="brand__descriptor-row">баскетб🏀льный портал</span><br> <span class="brand__descriptor-row">Москвы и области</span></span>
             </span>
         </a>
     </div>
