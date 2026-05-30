3deffce525012f765a9860bdce451712398374fe
Dmitry Losev <no-reply@mskba.ru>
2026-05-30 20:08:55 +0300

    lil fixes

--- resources/themes/mskba_light/views/partials/avatar.blade.php
+++ resources/themes/mskba_light/views/partials/avatar.blade.php
@@ -1,5 +1,8 @@
 @php
     $user = isset($user) ? $user : auth()->user();
+    $displayName = $user?->profile?->first_name
+        ? trim($user->profile->first_name . ($user->profile?->last_name ? ' ' . $user->profile->last_name : ''))
+        : $user->username;
 @endphp
 
 <div class="partial-wrapper partial-avatar text-center">
@@ -10,6 +13,6 @@
             <div class="avatar-placeholder rounded-circle avatar-lg d-flex align-items-center justify-content-center"></div>
         @endif
     </div>
-    <h5 class="card-title">{{ $user?->profile?->first_name }} {{ $user?->profile?->last_name }}</h5>
+    <h5 class="card-title">{{ $displayName }}</h5>
     <p class="card-text">{{ $user?->contacts?->primary_email }}</p>
 </div>
