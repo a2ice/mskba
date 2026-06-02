2dffe4875dd69ee67da1844cc8768145f53acd3d
Dmitry Losev <no-reply@mskba.ru>
2026-06-02 10:45:40 +0300

    logo

--- resources/themes/mskba_dark/views/partials/logo.blade.php
+++ resources/themes/mskba_dark/views/partials/logo.blade.php
@@ -1,4 +1,5 @@
 <a class="brand partial-wrapper partial-logo" href="{{ route('welcome') }}" aria-label="MSKBA">
+    <img class="brand__mark" src="{{ asset('images/logo-w.png') }}" alt="" width="1254" height="1254" aria-hidden="true">
     <span class="brand__text">
         <span class="brand__name">MSK<span>BA</span></span>
         <span class="brand__descriptor"><span class="brand__descriptor-row">баскетбольный портал</span><br> <span class="brand__descriptor-row">Москвы и области</span></span>
