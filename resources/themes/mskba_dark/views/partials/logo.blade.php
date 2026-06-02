c9f8216d119a49ccc69bfee0ba704d76d2fa7870
Dmitry Losev <no-reply@mskba.ru>
2026-06-02 11:08:17 +0300

    logo black ball t

--- resources/themes/mskba_dark/views/partials/logo.blade.php
+++ resources/themes/mskba_dark/views/partials/logo.blade.php
@@ -1,5 +1,5 @@
 <a class="brand partial-wrapper partial-logo" href="{{ route('welcome') }}" aria-label="MSKBA">
-    <img class="brand__mark" src="{{ asset('images/logo-with-black-ball.png') }}" alt="" width="1254" height="1254" aria-hidden="true">
+    <img class="brand__mark" src="{{ asset('images/logo-with-black-ball-t.png') }}" alt="" width="1254" height="1254" aria-hidden="true">
     <span class="brand__text">
         <span class="brand__name">MSK<span>BA</span></span>
         <span class="brand__descriptor"><span class="brand__descriptor-row">баскетбольный портал</span><br> <span class="brand__descriptor-row">Москвы и области</span></span>
