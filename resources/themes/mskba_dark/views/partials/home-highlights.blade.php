69e806ed266c998226a87b408a4be57ac759ddb7
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-01 18:25:23 +0300

    Link home venues highlight to venues catalog

--- resources/themes/mskba_dark/views/partials/home-highlights.blade.php
+++ resources/themes/mskba_dark/views/partials/home-highlights.blade.php
@@ -7,9 +7,14 @@
                 'trophy' => 'ti ti-trophy',
                 'shield' => 'ti ti-shield',
             ][$item['icon']] ?? 'ti ti-star';
+            $itemUrl = $item['title'] === 'Больше площадок' ? route('venues') : null;
         @endphp
 
-        <article class="home-highlight">
+        @if ($itemUrl)
+            <a class="home-highlight" href="{{ $itemUrl }}">
+        @else
+            <article class="home-highlight">
+        @endif
             <div class="home-highlight__icon" aria-hidden="true">
                 <i class="{{ $iconClass }}"></i>
             </div>
@@ -17,6 +22,10 @@
                 <h2 class="home-highlight__title">{{ $item['title'] }}</h2>
                 <p class="home-highlight__description">{{ $item['description'] }}</p>
             </div>
-        </article>
+        @if ($itemUrl)
+            </a>
+        @else
+            </article>
+        @endif
     @endforeach
 </div>
