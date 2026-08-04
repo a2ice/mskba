d38a5a576e3a6e3e4ccbddfa8cdda8b39df4df2a
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-04 11:57:52 +0300

    feat(events): enable lifecycle provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Modules\Contact\Infrastructure\Providers\ContactServiceProvider;
+use App\Modules\Event\Infrastructure\Providers\EventLifecycleServiceProvider;
 use App\Modules\Media\Infrastructure\Providers\MediaServiceProvider;
 use App\Modules\Notification\Infrastructure\Providers\NotificationServiceProvider;
 use App\Modules\Telegram\Infrastructure\Providers\TelegramServiceProvider;
@@ -12,6 +13,7 @@ return [
     AppServiceProvider::class,
     AccessServiceProvider::class,
     ContactServiceProvider::class,
+    EventLifecycleServiceProvider::class,
     MediaServiceProvider::class,
     NotificationServiceProvider::class,
     TelegramServiceProvider::class,
