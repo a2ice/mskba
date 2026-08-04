e29121302a143568bd4e19f65fc4a31782e6eea0
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-04 13:18:28 +0300

    feat(events): register roster player validation middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Modules\Event\Infrastructure\Http\Middleware\EnsureGameRosterContainsPlayers;
 use App\Modules\Identity\Infrastructure\Http\Middleware\RecordBrowserFingerprint;
 use App\Modules\Portal\Infrastructure\Http\Middleware\RecordOnlineUserPresence;
 use Illuminate\Foundation\Application;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->appendToGroup('web', [
             RecordBrowserFingerprint::class,
             RecordOnlineUserPresence::class,
+            EnsureGameRosterContainsPlayers::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
