56ede4ab72b93449f1955b4328d1753d02c6a44b
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-05 17:57:47 +0300

    Add team settings and join request permissions

--- app/Modules/Team/Domain/Enums/TeamPermissionEnum.php
+++ app/Modules/Team/Domain/Enums/TeamPermissionEnum.php
@@ -4,8 +4,10 @@ namespace App\Modules\Team\Domain\Enums;
 
 enum TeamPermissionEnum: string
 {
+    case EDIT_SETTINGS = 'team.settings.edit';
     case MANAGE_ROSTER = 'team.roster.manage';
     case INVITE_MEMBERS = 'team.members.invite';
+    case MANAGE_JOIN_REQUESTS = 'team.join_requests.manage';
     case MANAGE_ROLES = 'team.roles.manage';
     case MANAGE_PERMISSIONS = 'team.permissions.manage';
     case REMOVE_MEMBERS = 'team.members.remove';
@@ -13,8 +15,10 @@ enum TeamPermissionEnum: string
     public function label(): string
     {
         return match ($this) {
+            self::EDIT_SETTINGS => 'Редактировать настройки команды',
             self::MANAGE_ROSTER => 'Управлять основным составом и запасом',
             self::INVITE_MEMBERS => 'Приглашать участников',
+            self::MANAGE_JOIN_REQUESTS => 'Управлять заявками на вступление',
             self::MANAGE_ROLES => 'Назначать роли и капитана',
             self::MANAGE_PERMISSIONS => 'Выдавать права управления командой',
             self::REMOVE_MEMBERS => 'Исключать участников из команды',
