9f77b8b35ac6a3e38e48d21c3e89be8a94300ba0
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-05 11:17:39 +0300

    Register team user-context boundary middleware

--- app/Modules/Team/Infrastructure/Providers/TeamSportsServiceProvider.php
+++ app/Modules/Team/Infrastructure/Providers/TeamSportsServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Modules\Team\Infrastructure\Providers;
 use App\Modules\Admin\Presentation\Http\Controllers\AdminTeamsController;
 use App\Modules\Contract\Domain\Models\ContractMembership;
 use App\Modules\Team\Infrastructure\Http\Middleware\EnsureTeamMemberRemovalHierarchy;
+use App\Modules\Team\Infrastructure\Http\Middleware\EnsureTeamUserContext;
 use App\Modules\Team\Infrastructure\Observers\OwnerTeamMembershipObserver;
 use App\Modules\Team\Presentation\Http\Controllers\TeamManagementController;
 use App\Modules\Team\Presentation\Http\Controllers\TeamMemberSportsController;
@@ -16,6 +17,7 @@ final class TeamSportsServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ContractMembership::observe(OwnerTeamMembershipObserver::class);
+        $this->app['router']->pushMiddlewareToGroup('web', EnsureTeamUserContext::class);
         $this->app['router']->pushMiddlewareToGroup('web', EnsureTeamMemberRemovalHierarchy::class);
 
         Route::middleware(['web', 'auth'])->group(function (): void {
