2047e329f9bb6e5028baeb7a3feceb93bfd84959
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-07 10:45:25 +0300

    feat: register game live routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,7 +10,10 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        web: [
+            __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/game-live.php',
+        ],
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
