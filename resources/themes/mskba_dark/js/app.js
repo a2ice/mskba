cca239ab5524046e4484ff2a2eef4919e715ff5d
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-23 19:28:17 +0300

    fix: restore wizard state before initialization

--- resources/themes/mskba_dark/js/app.js
+++ resources/themes/mskba_dark/js/app.js
@@ -42,6 +42,7 @@ import './features/team-member-card-details.js';
 import './features/venue-show.js';
 import './features/venue-schedule-form.js';
 import './features/event-create-form.js';
+import './features/event-wizard-state-restore.js';
 import './features/event-wizard.js';
 import './features/event-wizard-venue.js';
 import './features/standalone-game-create.js';
