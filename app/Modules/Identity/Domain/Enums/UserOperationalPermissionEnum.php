67fcdcf929de4d95c606001e461743cfa7df1e45
a2ice <91248710+a2ice@users.noreply.github.com>
2026-08-24 10:53:04 +0300

    feat(identity): add event and tournament create permissions

--- app/Modules/Identity/Domain/Enums/UserOperationalPermissionEnum.php
+++ app/Modules/Identity/Domain/Enums/UserOperationalPermissionEnum.php
@@ -6,12 +6,24 @@ enum UserOperationalPermissionEnum: string
 {
     case CREATE_COORDINATION = 'coordination.create';
     case CREATE_TEAM = 'team.create';
+    case CREATE_EVENT = 'event.create';
+    case CREATE_TOURNAMENT = 'tournament.create';
 
     public function label(): string
     {
         return match ($this) {
             self::CREATE_COORDINATION => 'Создание опросов и согласований',
             self::CREATE_TEAM => 'Создание команд',
+            self::CREATE_EVENT => 'Создание мероприятий',
+            self::CREATE_TOURNAMENT => 'Создание турниров',
+        };
+    }
+
+    public function defaultAllowed(): bool
+    {
+        return match ($this) {
+            self::CREATE_EVENT, self::CREATE_TOURNAMENT => false,
+            default => true,
         };
     }
 }
