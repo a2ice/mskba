57a637751f6ce84310936cf29580c87f96e66f30
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-04 17:24:22 +0300

    feat(header): add sticky wrapper container

--- resources/themes/mskba_dark/views/partials/header.blade.php
+++ resources/themes/mskba_dark/views/partials/header.blade.php
@@ -1,24 +1,28 @@
 <header class="site-header partial-wrapper partial-header">
 
-    <div class="light-overlay"></div>
+    <div class="header-wrapper">
 
-    <div class="inner site-header-inner">
+        <div class="light-overlay"></div>
 
-        <div class="site-header-left header-cell">
-            @include('theme::partials.logo')
-        </div>
+        <div class="inner site-header-inner">
 
-        <div class="site-header-right header-cell">
+            <div class="site-header-left header-cell">
+                @include('theme::partials.logo')
+            </div>
 
-            @include('theme::partials.menu.main')
+            <div class="site-header-right header-cell">
 
-            <div class="site-auth header-cell" aria-label="Пользовательская навигация">
+                @include('theme::partials.menu.main')
 
-                @include('theme::partials.header-search')
-                @include('theme::partials.header-profile', ['isTelegramMiniApp' => $isTelegramMiniApp ?? false])
+                <div class="site-auth header-cell" aria-label="Пользовательская навигация">
 
-                <div class="header-cell">
-                    @include('theme::partials.menu.account-nav')
+                    @include('theme::partials.header-search')
+                    @include('theme::partials.header-profile', ['isTelegramMiniApp' => $isTelegramMiniApp ?? false])
+
+                    <div class="header-cell">
+                        @include('theme::partials.menu.account-nav')
+                    </div>
+
                 </div>
 
             </div>
