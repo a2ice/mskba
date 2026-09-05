a2de3ebeb1d9b95cfbaac3559e0deee48810664f
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-05 10:51:25 +0300

    Add ownership notification sources

--- app/Modules/Notification/Domain/Enums/UserNotificationSourceEnum.php
+++ app/Modules/Notification/Domain/Enums/UserNotificationSourceEnum.php
@@ -17,6 +17,10 @@ enum UserNotificationSourceEnum: string
     case VENUE_OWNERSHIP_CLAIM_SUBMITTED = 'venue.ownership_claim.submitted';
     case VENUE_OWNERSHIP_CLAIM_APPROVED = 'venue.ownership_claim.approved';
     case VENUE_OWNERSHIP_CLAIM_REJECTED = 'venue.ownership_claim.rejected';
+    case VENUE_OWNERSHIP_CLAIM_MESSAGE = 'venue.ownership_claim.message';
+    case VENUE_OWNERSHIP_STATUS_CHANGED = 'venue.ownership.status_changed';
+    case VENUE_USER_RESTRICTION_IMPOSED = 'venue.user_restriction.imposed';
+    case VENUE_USER_RESTRICTION_REVOKED = 'venue.user_restriction.revoked';
     case VENUE_MEMBERSHIP_GRANTED = 'venue.membership.granted';
     case VENUE_MEMBERSHIP_REVOKED = 'venue.membership.revoked';
     case VENUE_RENTAL_COORDINATION_JOINED = 'venue_rental.coordination.joined';
@@ -35,9 +39,13 @@ enum UserNotificationSourceEnum: string
             self::TEAM_JOIN_REQUEST_REJECTED => 'Отклонение заявки в команду',
             self::TEAM_JOIN_REQUEST_BLOCKED => 'Блокировка заявок в команду',
             self::TEAM_JOIN_REQUEST_UNBLOCKED => 'Разблокировка заявок в команду',
-            self::VENUE_OWNERSHIP_CLAIM_SUBMITTED => 'Отправка заявки на владение площадкой',
-            self::VENUE_OWNERSHIP_CLAIM_APPROVED => 'Одобрение заявки на владение площадкой',
-            self::VENUE_OWNERSHIP_CLAIM_REJECTED => 'Отклонение заявки на владение площадкой',
+            self::VENUE_OWNERSHIP_CLAIM_SUBMITTED => 'Отправка заявки на управление площадкой',
+            self::VENUE_OWNERSHIP_CLAIM_APPROVED => 'Одобрение заявки на управление площадкой',
+            self::VENUE_OWNERSHIP_CLAIM_REJECTED => 'Отклонение заявки на управление площадкой',
+            self::VENUE_OWNERSHIP_CLAIM_MESSAGE => 'Сообщение по заявке на управление площадкой',
+            self::VENUE_OWNERSHIP_STATUS_CHANGED => 'Изменение статуса владения площадкой',
+            self::VENUE_USER_RESTRICTION_IMPOSED => 'Ограничение заявок пользователя по площадке',
+            self::VENUE_USER_RESTRICTION_REVOKED => 'Снятие ограничения заявок пользователя по площадке',
             self::VENUE_MEMBERSHIP_GRANTED => 'Выдача коммерческой роли площадки',
             self::VENUE_MEMBERSHIP_REVOKED => 'Отзыв коммерческой роли площадки',
             self::VENUE_RENTAL_COORDINATION_JOINED => 'Вступление в сбор по аренде',
