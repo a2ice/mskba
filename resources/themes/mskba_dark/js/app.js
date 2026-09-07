27a13ed8fa6cc3211b5932debab5bb7b160cc0e6
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-07 15:07:44 +0300

    feat(home): wire event search parameters flow

--- resources/themes/mskba_dark/js/app.js
+++ resources/themes/mskba_dark/js/app.js
@@ -95,6 +95,7 @@ import './features/home-hero-parallax.js';
 import './features/home-event-venue-bridge.js';
 import './features/home-venue-flow-fixes.js';
 import './features/home-flow-navigation.js';
+import './features/home-event-search-parameters.js';
 import './features/reactions.js';
 import './features/admin-action-modals.js';
 import './features/admin-venue-bulk-actions.js';
