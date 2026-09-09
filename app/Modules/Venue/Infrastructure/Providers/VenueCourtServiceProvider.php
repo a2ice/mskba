9526a0119752a6939125241a93bb064dcb155781
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-10 02:28:47 +0300

    Task 162: register court routes via provider

--- app/Modules/Venue/Infrastructure/Providers/VenueCourtServiceProvider.php
+++ app/Modules/Venue/Infrastructure/Providers/VenueCourtServiceProvider.php
@@ -12,17 +12,19 @@ final class VenueCourtServiceProvider extends RouteServiceProvider
     {
         parent::boot();
 
-        Route::middleware(['web', 'auth'])->group(function (): void {
-            Route::get('/account/venues/{venue}/courts', [VenueCourtController::class, 'index'])
-                ->name('account.venues.courts.index');
-            Route::post('/account/venues/{venue}/courts', [VenueCourtController::class, 'store'])
-                ->name('account.venues.courts.store');
-            Route::put('/account/venues/{venue}/courts/{court}', [VenueCourtController::class, 'update'])
-                ->name('account.venues.courts.update');
-            Route::post('/account/venues/{venue}/courts/{court}/primary', [VenueCourtController::class, 'makePrimary'])
-                ->name('account.venues.courts.primary');
-            Route::delete('/account/venues/{venue}/courts/{court}', [VenueCourtController::class, 'destroy'])
-                ->name('account.venues.courts.destroy');
+        $this->routes(function (): void {
+            Route::middleware(['web', 'auth'])->group(function (): void {
+                Route::get('/account/venues/{venue}/courts', [VenueCourtController::class, 'index'])
+                    ->name('account.venues.courts.index');
+                Route::post('/account/venues/{venue}/courts', [VenueCourtController::class, 'store'])
+                    ->name('account.venues.courts.store');
+                Route::put('/account/venues/{venue}/courts/{court}', [VenueCourtController::class, 'update'])
+                    ->name('account.venues.courts.update');
+                Route::post('/account/venues/{venue}/courts/{court}/primary', [VenueCourtController::class, 'makePrimary'])
+                    ->name('account.venues.courts.primary');
+                Route::delete('/account/venues/{venue}/courts/{court}', [VenueCourtController::class, 'destroy'])
+                    ->name('account.venues.courts.destroy');
+            });
         });
     }
 }
