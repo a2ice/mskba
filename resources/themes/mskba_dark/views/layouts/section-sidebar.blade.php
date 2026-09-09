0ecdf57c2013c1be41b6f000824f16fd9311ea3d
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-10 02:43:37 +0300

    Task 164: allow heading title suffix controls

--- resources/themes/mskba_dark/views/layouts/section-sidebar.blade.php
+++ resources/themes/mskba_dark/views/layouts/section-sidebar.blade.php
@@ -65,6 +65,12 @@
                                                 data-tooltip-variant="title"
                                             @endif
                                         >{{ $contentTitle }}</h1>
+
+                                        @hasSection('section-heading-title-suffix')
+                                            <div class="section-content-heading__title-suffix">
+                                                @yield('section-heading-title-suffix')
+                                            </div>
+                                        @endif
                                     </div>
 
                                     @hasSection('section-heading-action')
