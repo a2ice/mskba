c622b4f54b758ff200780315e82f4eb67f93086b
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-12 14:41:40 +0300

    feat(venues): add category navigation slot

---
+++ resources/themes/mskba_dark/views/pages/venues/partials/catalog-navigation.blade.php
@@ -0,0 +1,8 @@
+<div class="default-category-sidebar__section">
+    <h2 class="default-category-sidebar__title">Площадки</h2>
+    <nav class="default-category-sidebar__nav" aria-label="Навигация по площадкам">
+        <a class="is-active" href="{{ route('venues') }}" aria-current="page">
+            <span>Все площадки</span><i class="ti ti-map-pin" aria-hidden="true"></i>
+        </a>
+    </nav>
+</div>
