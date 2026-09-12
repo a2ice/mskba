d487d851ffc9f163ce65ec549845ede541efc712
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-12 16:18:51 +0300

    isolate new event catalog cards from legacy styles

--- resources/themes/mskba_dark/views/pages/events/partials/catalog-item.blade.php
+++ resources/themes/mskba_dark/views/pages/events/partials/catalog-item.blade.php
@@ -26,8 +26,6 @@
 @endphp
 
 <article @class([
-    'catalog-card',
-    'event-catalog-card',
     'event-category-item',
     'event-category-item--'.$mode,
     'is-past' => $isPast,
@@ -44,7 +42,7 @@
             @endif
         </div>
 
-        <h2 class="catalog-card__title event-category-item__title"><a href="{{ $eventUrl }}">{{ $event->title }}</a></h2>
+        <h2 class="event-category-item__title"><a href="{{ $eventUrl }}">{{ $event->title }}</a></h2>
 
         <div class="event-category-item__meta">
             @if($latitude !== null && $longitude !== null)
