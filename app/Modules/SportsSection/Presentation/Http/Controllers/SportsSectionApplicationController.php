1679b93c37b609c9a85fff2bde96fb803a744756
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-13 22:58:05 +0300

    Add section application controller

---
+++ app/Modules/SportsSection/Presentation/Http/Controllers/SportsSectionApplicationController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Modules\SportsSection\Presentation\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+final class SportsSectionApplicationController extends Controller
+{
+}
