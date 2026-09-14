2a60aaf212bf34011ba4b24f607021d0bf8677dc
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-14 14:40:32 +0300

    Task 178: load sports section catalog feature

--- resources/themes/mskba_dark/js/app.js
+++ resources/themes/mskba_dark/js/app.js
@@ -52,6 +52,7 @@ import './features/entity-predictive-search.js';
 import './features/venue-map.js';
 import './features/venue-catalog.js';
 import './features/team-catalog.js';
+import './features/sports-section-catalog.js';
 import './features/team-name-suggestion.js';
 import './features/team-pending-invitations.js';
 import './features/team-management.js';
