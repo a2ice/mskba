f065cd0cde5865a92823036f31759132a32d0686
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-15 11:35:29 +0300

    Unify VK quick-login presentation

--- resources/themes/mskba_dark/views/partials/auth/vk-login.blade.php
+++ resources/themes/mskba_dark/views/partials/auth/vk-login.blade.php
@@ -1,16 +1,16 @@
 @if(trim((string) config('vk.app_id')) !== '')
     <div class="auth-vk-login">
         <div class="auth-telegram-login__separator" aria-hidden="true">
-            <span>или через VK ID</span>
+            <span>или</span>
         </div>
 
         <a
-            class="btn btn--sm auth-vk-login__button"
+            class="auth-social-login__button auth-social-login__button--vk"
             href="{{ route('auth.vk.start', ! empty($authRedirectTo) ? ['redirect_to' => $authRedirectTo] : []) }}"
             data-vk-auth-url="{{ route('auth.vk.start', [], false) }}"
         >
-            <span class="auth-vk-login__icon" aria-hidden="true">VK</span>
-            Войти через VK ID
+            <span class="auth-social-login__icon auth-vk-login__icon" aria-hidden="true">VK</span>
+            <span>VKontakte</span>
         </a>
     </div>
 @endif
