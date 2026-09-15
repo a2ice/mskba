8905b5f3e32ff41e977c27d08dae05973f89e1f9
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-15 11:56:36 +0300

    Show business rules in creation guides

--- resources/themes/mskba_dark/views/partials/creation-guide.blade.php
+++ resources/themes/mskba_dark/views/partials/creation-guide.blade.php
@@ -7,6 +7,7 @@
         <summary><i class="ti ti-help" aria-hidden="true"></i><span>{{ $helpGuide['heading'] }}</span><i class="ti ti-chevron-down creation-guide__arrow" aria-hidden="true"></i></summary>
         <div class="creation-guide__body">
             <p>{{ $helpGuide['intro'] }}</p>
+            @include('theme::partials.creation-requirements', ['requirements' => $helpGuide['requirements'] ?? []])
             <a href="{{ route('faq.creation', ['topic' => $helpTopic]) }}" target="_blank" rel="noopener">Полная инструкция в FAQ <i class="ti ti-external-link" aria-hidden="true"></i></a>
         </div>
     </details>
