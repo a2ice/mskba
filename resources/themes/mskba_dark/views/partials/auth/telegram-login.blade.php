57fe1171dbec5219e61590d96026eefe25b3b787
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-15 11:35:21 +0300

    Polish Telegram quick-login presentation

--- resources/themes/mskba_dark/views/partials/auth/telegram-login.blade.php
+++ resources/themes/mskba_dark/views/partials/auth/telegram-login.blade.php
@@ -12,22 +12,25 @@
         data-telegram-bot-status-url="{{ route('auth.telegram.bot.status', [], false) }}"
     >
         <div class="auth-telegram-login__separator" aria-hidden="true">
-            <span>или через Telegram</span>
+            <span>или быстрый вход через</span>
         </div>
 
-        <div class="auth-telegram-login__option">
-            <span class="auth-telegram-login__option-label">Быстрый вход</span>
-            <div class="auth-telegram-login__widget">
-                <script
-                    async
-                    src="https://telegram.org/js/telegram-widget.js?22"
-                    data-telegram-login="{{ $telegramBotUsername }}"
-                    data-size="large"
-                    data-radius="10"
-                    data-userpic="false"
-                    data-onauth="mskbaTelegramLogin(user)"
-                ></script>
-            </div>
+        <div class="auth-telegram-login__widget">
+            <span class="auth-social-login__button auth-social-login__button--telegram" aria-hidden="true">
+                <svg class="auth-social-login__icon auth-telegram-login__icon" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="m9.78 18.65.28-4.23 7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3 3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.81c-.19.91-.74 1.13-1.5.71L12.6 16.3l-2 1.93c-.23.23-.42.42-.82.42Z"/>
+                </svg>
+                <span>Telegram</span>
+            </span>
+            <script
+                async
+                src="https://telegram.org/js/telegram-widget.js?22"
+                data-telegram-login="{{ $telegramBotUsername }}"
+                data-size="large"
+                data-radius="10"
+                data-userpic="false"
+                data-onauth="mskbaTelegramLogin(user)"
+            ></script>
         </div>
 
         {{--
