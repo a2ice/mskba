02ab43f8b00d3d500b9201c3f7905782b84e1c1e
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-18 23:40:59 +0300

    legal: register legal routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: [
             __DIR__.'/../routes/web.php',
+            __DIR__.'/../routes/legal.php',
             __DIR__.'/../routes/faq-content.php',
             __DIR__.'/../routes/admin-geography.php',
             __DIR__.'/../routes/home-location.php',
