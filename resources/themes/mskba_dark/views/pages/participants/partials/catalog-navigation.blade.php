a3eef5ac93b67644109ae1303b00a103cd4ea645
a2ice <91248710+a2ice@users.noreply.github.com>
2026-09-19 11:41:07 +0300

    Task 205: add participant catalog view

---
+++ resources/themes/mskba_dark/views/pages/participants/partials/catalog-navigation.blade.php
@@ -0,0 +1,21 @@
+@php
+    $persistent = array_filter([
+        'q' => filled($filters['q'] ?? null) ? $filters['q'] : null,
+        'view' => request('view') === 'list' ? 'list' : null,
+    ]);
+@endphp
+
+<nav class="default-category-sidebar__nav" aria-label="Навигация по участникам">
+    <a @class(['is-active' => request()->routeIs('participants.players')]) href="{{ route('participants.players', $persistent) }}" @if(request()->routeIs('participants.players')) aria-current="page" @endif>
+        <span>Игроки</span><i class="ti ti-ball-basketball" aria-hidden="true"></i>
+    </a>
+    <a @class(['is-active' => request()->routeIs('participants.coaches')]) href="{{ route('participants.coaches', $persistent) }}" @if(request()->routeIs('participants.coaches')) aria-current="page" @endif>
+        <span>Тренеры</span><i class="ti ti-whistle" aria-hidden="true"></i>
+    </a>
+    <a @class(['is-active' => request()->routeIs('participants.index')]) href="{{ route('participants.index', $persistent) }}" @if(request()->routeIs('participants.index')) aria-current="page" @endif>
+        <span>Все участники</span><i class="ti ti-users" aria-hidden="true"></i>
+    </a>
+    <a href="{{ route('teams.index') }}">
+        <span>Команды</span><i class="ti ti-users-group" aria-hidden="true"></i>
+    </a>
+</nav>
